38fd9e70d364cdda34f34ebe0908a44b1c4b9f94
Willian Silvestre <silvestrewillian073@gmail.com>
2023-06-24 23:41:05 -0300

    26

--- senha/recuperar_senha_envia.php
+++ senha/recuperar_senha_envia.php
@@ -1,51 +1,38 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-require_once('../valida_login/PHPMailer/src/Exception.php');
-require_once('../valida_login/PHPMailer/src/PHPMailer.php');
-require_once('../valida_login/PHPMailer/src/SMTP.php');
-require_once('../bd/conecta_bd.php');
+require_once("../bd/conecta_bd.php");
 
 if (isset($_POST['validar-email'])) {
+
+
+    
     $email = $_POST['email'];
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $_SESSION['texto_erro'] = 'E-mail inválido';
-	        header ("Location:../index.php");
+        header("Location:../index.php");
     } else {
         $novasenha = substr(md5(time()), 0, 6);
 
-        $mail = new PHPMailer(true);
+        $to = $email;
+        $subject = 'Sua nova senha';
+        $message = 'Sua nova senha: ' . $novasenha;
 
-        try {
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = 'josesilva9856443439848944@gmail.com';
-            $mail->Password = '5^5d38soJktZ';
-            $mail->SMTPSecure = 'tls';
-            $mail->Port = 587;
+        $headers = "From: remetente@example.com\r\n";
+        $headers .= "Reply-To: remetente@example.com\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-            $mail->setFrom('josesilva9856443439848944@gmail.com', 'Ruralgest');
-            $mail->addAddress($email);
-
-            $mail->isHTML(true);
-            $mail->Subject = 'Sua nova senha';
-            $mail->Body = 'Sua nova senha: ' . $novasenha;
-
-            $mail->send();
-
+        if (mail($to, $subject, $message, $headers)) {
             $sql = "UPDATE usuario SET senha = '$novasenha' WHERE email ='$email'";
             $consulta = $db->prepare($sql);
             $consulta->execute();
 
             $_SESSION['texto_sucesso'] = 'A senha foi alterada no sistema.';
-            header ("Location:editar_senha.php");
-
-        } catch (Exception $e) {
+            header("Location:recuperar_senha.php");
+        } else {
             $_SESSION['texto_erro'] = 'A senha não foi alterada no sistema!';
-	        header ("Location:../index.php");
+            header("Location:recuperar_senha.php");
         }
     }
 }
+?>
